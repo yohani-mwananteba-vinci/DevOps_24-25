d31dfa936c0880acc0e678ee509cf8acf2357d2c
Gaetan Van den Berghe <gaetan.vandenberghe@student.uclouvain.be>
2024-10-10 16:10:11 +0200

    refactore

--- routes/users.js
+++ routes/users.js
@@ -24,33 +24,19 @@ router.post('/login', (req, res, next) => {
     console.log("User found" + JSON.stringify(userFound));
     if (userFound) {
         if (userFound.active == false) {
-            req.session.errors = "Compte désactivé";
-            res.redirect('/users');
+            verificationCompteDesactiver(req, res);
         }
         else {
             if (bcrypt.compareSync(req.body.userPassword, userFound.password)) {
-                console.log("password correct");
-                req.session.login = req.body.userLogin;
-                req.session.connected = true;
-                if (userFound.admin) {
-                    req.session.admin = true;
-                    res.redirect('/admin');
-                } else {
-                    req.session.admin = false;
-                    res.redirect('/members');
-                }
+                VerificationPassword(req, userFound, res);
             }
             else {
-                console.log("bad password");
-                req.session.errors = "Mot de passe incorrect";
-                res.redirect('/users');
+                badPassword(req, res);
             }
         }
     }
     else {
-        console.log("bad user");
-        req.session.errors = "Utilisateur inconnu";
-        res.redirect('/users');
+        badUser(req, res);
     }
 });
 
@@ -96,3 +82,33 @@ router.post('/add', (req, res, next) => {
 });
 
 module.exports = router;
+
+function badUser(req, res) {
+    console.log("bad user");
+    req.session.errors = "Utilisateur inconnu";
+    res.redirect('/users');
+}
+
+function badPassword(req, res) {
+    console.log("bad password");
+    req.session.errors = "Mot de passe incorrect";
+    res.redirect('/users');
+}
+
+function VerificationPassword(req, userFound, res) {
+    console.log("password correct");
+    req.session.login = req.body.userLogin;
+    req.session.connected = true;
+    if (userFound.admin) {
+        req.session.admin = true;
+        res.redirect('/admin');
+    } else {
+        req.session.admin = false;
+        res.redirect('/members');
+    }
+}
+
+function verificationCompteDesactiver(req, res) {
+    req.session.errors = "Compte désactivé";
+    res.redirect('/users');
+}
